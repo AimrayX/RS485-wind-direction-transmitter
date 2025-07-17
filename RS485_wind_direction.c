a92d541e363711032e0eb3b7f15f249f7cecce47
AimrayX <timothy.hall@gmx.ch>
2025-07-17 16:16:32 +0200

    finished functionality and tested it

--- RS485_wind_direction.c
+++ RS485_wind_direction.c
@@ -3,6 +3,8 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <errno.h>
+#include <math.h>
 
 int rs485_wind_direction_begin(rs485_wind_direction_t *dev) {
     rs485_wind_direction_init(dev);
@@ -16,25 +18,33 @@ int rs485_wind_direction_init(rs485_wind_direction_t *dev) {
         return -1;
     }
 
-    // Set the Modbus slave ID
     modbus_set_slave(dev->ctx, 2);
 
-    // Open the serial port
     if (modbus_connect(dev->ctx) == -1) {
         fprintf(stderr, "Connection failed: %s\n", modbus_strerror(errno));
         modbus_free(dev->ctx);
         return -1;
     }
+    return 0;
 }
 
 int rs485_wind_direction_read_register(rs485_wind_direction_t *dev) {
-     dev->rc = modbus_read_registers(dev->ctx, 0x0000, 1, tab_reg);
-    if (dev->rc == -1) {
+    uint16_t tab_reg[32];
+    int rc = modbus_read_registers(dev->ctx, 0x0000, 1, tab_reg);
+    if (rc == -1) {
         fprintf(stderr, "Failed to read: %s\n", modbus_strerror(errno));
-        modbus_close(dev->ctx);
-        modbus_free(dev->ctx);
         return -1;
     }
+    
+    // calculate with correction formula since mine was oddly of by roughly this polinomial function
+    double result = 10 * round((-0.00006 * pow((double)tab_reg[0], 2.0) + 1.20048 * (double)tab_reg[0] + 10.76594) / 100.0);
+    if (result > 355.0) {
+        dev->wind_direction = 0.0;
+    } else {
+        dev->wind_direction = result;
+    }
+    
+    return 0;
 }
 
 int rs485_wind_direction_close(rs485_wind_direction_t *dev) {
--- RS485_wind_direction.h
+++ RS485_wind_direction.h
@@ -1,19 +1,18 @@
-#ifndef _RS485_WIND_DIRECTION_H_
-#define _RS485_WIND_DIRECTION_H_
+#ifndef RS485_WIND_DIRECTION_H
+#define RS485_WIND_DIRECTION_H
 
-#include <modbus.h>
+#include <modbus/modbus.h>
 
 typedef struct rs485_wind_direction
 {
     modbus_t *ctx;
-    uint16_t tab_reg[32];
-    int rc;
+    double wind_direction;
 } rs485_wind_direction_t;
 
-int rs485_wind_direction_begin();
-int rs485_wind_direction_init();
-int rs485_wind_direction_read_register();
-int rs485_wind_direction_close();
+int rs485_wind_direction_begin(rs485_wind_direction_t *dev);
+int rs485_wind_direction_init(rs485_wind_direction_t *dev);
+int rs485_wind_direction_read_register(rs485_wind_direction_t *dev);
+int rs485_wind_direction_close(rs485_wind_direction_t *dev);
 
 
 #endif
